9fdc3b2fdffc08b468f2f355a6a4b05e60885f70
abdulkadirkraa <karaabdulkadir498@gmail.com>
2024-12-22 01:11:34 +0300

    cardviewpager structure added to home screen

--- app/src/main/java/com/abdulkadirkara/paparauiclonecompose/ui/screens/ScreenHome.kt
+++ app/src/main/java/com/abdulkadirkara/paparauiclonecompose/ui/screens/ScreenHome.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
@@ -15,21 +14,18 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.abdulkadirkara.paparauiclonecompose.R
 import com.abdulkadirkara.paparauiclonecompose.ui.screens.components.CustomTopAppBar
 import com.abdulkadirkara.paparauiclonecompose.ui.screens.components.Story
 import com.abdulkadirkara.paparauiclonecompose.ui.screens.components.StoryList
-import com.abdulkadirkara.paparauiclonecompose.ui.screens.components.upviewpager.CardPagerCommon
 import com.abdulkadirkara.paparauiclonecompose.ui.screens.components.upviewpager.CardPagerFifth
 import com.abdulkadirkara.paparauiclonecompose.ui.screens.components.upviewpager.CardPagerFirst
 import com.abdulkadirkara.paparauiclonecompose.ui.screens.components.upviewpager.CardPagerFourth
@@ -44,7 +40,7 @@ import com.google.accompanist.pager.rememberPagerState
 @OptIn(ExperimentalPagerApi::class)
 @Preview(showSystemUi = true)
 @Composable
-fun ScreenHome(){
+fun ScreenHome() {
 
     // ScrollState oluştur
     val scrollState = rememberScrollState()
@@ -53,61 +49,76 @@ fun ScreenHome(){
         topBar = {
             CustomTopAppBar()
         }
-    ) {paddingValues ->
+    ) { paddingValues ->
         Column(
-            modifier = Modifier.padding(paddingValues)
+            modifier = Modifier
+                .padding(paddingValues)
                 .fillMaxSize()
                 .background(Color.White)
                 .verticalScroll(scrollState),
         ) {
 
             //Hikayeler
-            val storyList = listOf(
-                Story(R.drawable.img_wrap_up_papara_cachback, "Cashback"),
-                Story(R.drawable.img, "Duyurular"),
-                Story(R.drawable.img_wrap_up_invite_friends, "Davet Et,\nKazan"),
-                Story(R.drawable.img_wrap_up_international_transfer, "Yurt Dışı Para\nTransferi"),
-                Story(R.drawable.img_wrap_up_papara_chat, "Sohbetler"),
-                Story(R.drawable.img_wrap_up_papara_international, "Seyahat\nAyrıcalıkları"),
-                Story(R.drawable.img_1, "Kıymetli\nMadenler"),
-                Story(R.drawable.img_wrap_up_papara_iban, "Aylık Özet"),
-            )
-
-            StoryList(stories = storyList)
+            HomeScreenStories()
 
             //Card ViewPager
-            val pages = listOf(
-                CardPagerFirst(),
-                CardPagerSecond(),
-                CardPagerThird(),
-                CardPagerFourth(),
-                CardPagerFifth(),
-                CardPagerLast()
-            )
-
-            // Setting up Pager State
-            val pagerState = rememberPagerState(pageCount = pages.size)
-
-            // Horizontal Pager
-            Box {
-                HorizontalPager(
-                    state = pagerState,
-                    modifier = Modifier
-                        .fillMaxWidth() // Set the pager to fill the entire width
-                ) {
-                }
-            }
-
-            // Page Indicator
-            PageIndicator(
-                pageCount = pages.size,
-                currentPage = pagerState.currentPage,
-                modifier = Modifier
-                    .wrapContentWidth()
-                    .align(Alignment.CenterHorizontally)
-            )
+            HomeScreenVierPagerCards()
+            
         }
     }
+}
+
+@Composable
+fun HomeScreenStories(){
+    val storyList = listOf(
+        Story(R.drawable.img_wrap_up_papara_cachback, "Cashback"),
+        Story(R.drawable.img, "Duyurular"),
+        Story(R.drawable.img_wrap_up_invite_friends, "Davet Et,\nKazan"),
+        Story(R.drawable.img_wrap_up_international_transfer, "Yurt Dışı Para\nTransferi"),
+        Story(R.drawable.img_wrap_up_papara_chat, "Sohbetler"),
+        Story(R.drawable.img_wrap_up_papara_international, "Seyahat\nAyrıcalıkları"),
+        Story(R.drawable.img_1, "Kıymetli\nMadenler"),
+        Story(R.drawable.img_wrap_up_papara_iban, "Aylık Özet"),
+    )
+
+    StoryList(stories = storyList)
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun HomeScreenVierPagerCards(){
+
+
+    // Setting up Pager State
+    val pagerState = rememberPagerState(pageCount = 6)
+
+    // Horizontal Pager
+    Box(modifier = Modifier.wrapContentSize()) {
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(12.dp)
+        ) { page ->
+            // Her bir sayfada farklı bir içerik gösterelim
+            when(page){
+                0 -> CardPagerFirst()
+                1 -> CardPagerSecond()
+                2 -> CardPagerThird()
+                3 -> CardPagerFourth()
+                4 -> CardPagerFifth()
+                5 -> CardPagerLast()
+            }
+        }
+    }
+
+    // Page Indicator
+    PageIndicator(
+        pageCount = 6,
+        currentPage = pagerState.currentPage,
+        modifier = Modifier
+            .wrapContentWidth()
+    )
 }
 
 @Composable
@@ -115,7 +126,7 @@ fun PageIndicator(pageCount: Int, currentPage: Int, modifier: Modifier) {
     Row(
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
+        modifier = modifier.fillMaxWidth()
     ) {
         repeat(pageCount) {
             IndicatorDots(isSelected = it == currentPage, modifier = modifier)
