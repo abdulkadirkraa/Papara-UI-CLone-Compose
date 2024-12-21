d105a7151035058003593d71def54bc4de01e802
abdulkadirkraa <karaabdulkadir498@gmail.com>
2024-12-21 21:36:48 +0300

    cardviewpagerfirst optimized

--- app/src/main/java/com/abdulkadirkara/paparauiclonecompose/ui/screens/components/upviewpager/CardPagerFirst.kt
+++ app/src/main/java/com/abdulkadirkara/paparauiclonecompose/ui/screens/components/upviewpager/CardPagerFirst.kt
@@ -2,13 +2,16 @@ package com.abdulkadirkara.paparauiclonecompose.ui.screens.components.upviewpage
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,6 +22,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -27,7 +31,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,26 +39,27 @@ import androidx.compose.ui.unit.sp
 import com.abdulkadirkara.paparauiclonecompose.R
 import com.abdulkadirkara.paparauiclonecompose.ui.theme.investment_medium
 import com.abdulkadirkara.paparauiclonecompose.ui.theme.investment_semibold
-import com.abdulkadirkara.paparauiclonecompose.ui.theme.pager_stroke
-import com.abdulkadirkara.paparauiclonecompose.ui.theme.papara_black
-
+import com.abdulkadirkara.paparauiclonecompose.ui.theme.lightGrayDrawableMenu
 
 @Preview(showBackground = true)
 @Composable
-fun CardPagerFirst(){
+fun CardPagerFirst() {
     Card(
-        modifier = Modifier.fillMaxWidth()
-            .height(265.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(240.dp)
+            .padding(16.dp),
+        shape = RoundedCornerShape(12.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        colors = CardDefaults.cardColors(containerColor = lightGrayDrawableMenu)
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.drawable_lighter_grey_bg),
-            contentDescription = "",
-            contentScale = ContentScale.FillBounds
-        )
-        Column(modifier = Modifier.padding(16.dp),
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
             horizontalAlignment = Alignment.Start,
-            verticalArrangement = Arrangement.Center) {
-            // Tr bayrak ikon ve text
+            verticalArrangement = Arrangement.Top
+        ) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.Start,
@@ -63,84 +67,83 @@ fun CardPagerFirst(){
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.ic_flag_tl),
-                    contentDescription = "",
-                    contentScale = ContentScale.Inside,
+                    contentDescription = "Türk Lirası Bayrağı",
                     modifier = Modifier
                         .size(20.dp)
                         .clip(CircleShape)
-                        .border(
-                            2.dp,
-                            color = Color.White,
-                            CircleShape
-                        )
+                        .border(2.dp, color = Color.White, shape = CircleShape)
                 )
                 Spacer(modifier = Modifier.width(12.dp))
                 Text(
                     text = "Türk Lirası Hesabı",
-                    fontFamily = investment_medium,
-                    color = papara_black,
-                    fontSize = 12.sp
-                )
-
-            }
-
-            //Divider
-            HorizontalDivider(
-                modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp),
-                thickness = 1.dp,
-                color = pager_stroke
-            )
-
-            Spacer(modifier = Modifier.height(32.dp))
-
-            //hesap
-            Text(
-                text = "₺999,92",
-                fontFamily = investment_semibold,
-                color = papara_black,
-                fontSize = 28.sp
-            )
-
-            //Iban kısmı
-            Row(
-                horizontalArrangement = Arrangement.Start,
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .padding(top = 4.dp)
-            ) {
-                Text(
-                    text = "IBAN ın:",
-                    fontSize = 10.sp,
-                    fontFamily = investment_medium
-                )
-
-                Text(
-                    text = "TR11 3452 6785 7896 9029 4680 11",
-                    fontSize = 10.sp,
-                    fontFamily = investment_medium,
-                    textDecoration = TextDecoration.Underline,
-                    modifier = Modifier.padding(start = 4.dp),
+                    style = MaterialTheme.typography.bodySmall.copy(
+                        fontFamily = investment_medium,
+                        fontSize = 12.sp
+                    ),
                     color = Color.Black
                 )
             }
 
-            //İKi icon button
+            HorizontalDivider(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp),
+                thickness = 1.dp,
+                color = Color.Gray
+            )
+
+            Text(
+                text = "₺999,92",
+                style = MaterialTheme.typography.titleLarge.copy(
+                    fontFamily = investment_semibold,
+                    fontSize = 28.sp
+                ),
+                color = Color.Black
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
             Row(
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = "IBAN ın:",
+                    style = MaterialTheme.typography.bodySmall,
+                    fontSize = 10.sp,
+                    fontFamily = investment_medium
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "TR11 3452 6785 7896 9029 4680 11",
+                    style = MaterialTheme.typography.bodySmall.copy(
+                        fontSize = 10.sp,
+                        fontFamily = investment_medium
+                    ),
+                    textDecoration = TextDecoration.Underline,
+                    color = Color.Black
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
             ) {
                 CustomButton(
                     onClick = { /* İşlem */ },
                     buttonText = "Yatır / Çek",
-                    drawableEnd = R.drawable.ic_dashboard_wallet
+                    drawableEnd = R.drawable.ic_dashboard_wallet,
+                    modifier = Modifier.weight(1f)
                 )
                 CustomButton(
-                    onClick = {},
+                    onClick = { /* İşlem */ },
                     buttonText = "Gönder",
-                    drawableEnd = R.drawable.ic_dashboard_send
+                    drawableEnd = R.drawable.ic_dashboard_send,
+                    modifier = Modifier.weight(1f)
                 )
             }
-
         }
     }
 }
@@ -149,37 +152,41 @@ fun CardPagerFirst(){
 fun CustomButton(
     onClick: () -> Unit,
     buttonText: String,
-    @DrawableRes drawableEnd: Int
+    @DrawableRes drawableEnd: Int,
+    modifier: Modifier = Modifier
 ) {
-    Button(
-        onClick = onClick,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(IntrinsicSize.Min) // `wrap_content`'e benzer
-            .padding(end = 4.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Transparent // Drawable arka plan kullanacağız
-        ),
-        shape = RoundedCornerShape(8.dp), // Drawable'daki köşe yuvarlama ile uyumlu yap
-        contentPadding = PaddingValues(12.dp) // İçerik için padding
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(8.dp)) // Yuvarlak köşeleri uygula
+            .background(Color.White) // Beyaz arka plan
+            .border(1.dp, Color.LightGray, RoundedCornerShape(8.dp)) // Hafif gri kenarlık
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+        Button(
+            onClick = onClick,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+            contentPadding = PaddingValues(12.dp),
+            shape = RoundedCornerShape(8.dp) // Yuvarlak köşe için aynı şekil
         ) {
-            Text(
-                text = buttonText,
-                style = MaterialTheme.typography.bodyMedium,
-                color = Color.Black,
-                modifier = Modifier.weight(1f)
-            )
-
-            Spacer(modifier = Modifier.width(8.dp)) // Metin ve simge arasında boşluk
-            Image(
-                painter = painterResource(id = drawableEnd),
-                contentDescription = null,
-                modifier = Modifier.size(24.dp) // Simge boyutunu ayarla
-            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = buttonText,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.Black,
+                    modifier = Modifier.weight(1f)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Image(
+                    painter = painterResource(id = drawableEnd),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
         }
     }
 }
