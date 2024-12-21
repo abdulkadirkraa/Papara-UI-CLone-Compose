14f1e74d49af27b1812014f6451d4087a39496cf
abdulkadirkraa <karaabdulkadir498@gmail.com>
2024-12-21 22:17:46 +0300

    cardviewpagerlast optimized

--- app/src/main/java/com/abdulkadirkara/paparauiclonecompose/ui/screens/components/upviewpager/CardPagerLast.kt
+++ app/src/main/java/com/abdulkadirkara/paparauiclonecompose/ui/screens/components/upviewpager/CardPagerLast.kt
@@ -2,17 +2,17 @@ package com.abdulkadirkara.paparauiclonecompose.ui.screens.components.upviewpage
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
@@ -22,7 +22,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -34,142 +33,133 @@ import com.abdulkadirkara.paparauiclonecompose.R
 
 @Preview(showBackground = true)
 @Composable
-fun CardPagerLast(){
-    Card (modifier = Modifier.fillMaxWidth()
-        .height(265.dp),) {
-
-        Image(
-            painter = painterResource(id = R.drawable.drawable_lighter_grey_bg),
-            contentDescription = "",
-            contentScale = ContentScale.FillBounds
-        )
-
-        Column(
+fun CardPagerLastUpdated() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(240.dp)
+            .padding(16.dp),
+        shape = RoundedCornerShape(20.dp)
+    ) {
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
+                .fillMaxSize()
                 .background(Color.LightGray)
         ) {
-            // Header Text (Tüm Varlıklarım)
-            Text(
-                text = "Tüm Varlıklarım",
-                fontFamily = FontFamily(Font(R.font.papara_regular)),
-                fontSize = 14.sp,
-                color = Color.Black,
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 16.dp, top = 16.dp)
-                    .background(Color.White, shape = RoundedCornerShape(20.dp))
-                    .border(2.dp, Color.White, shape = RoundedCornerShape(20.dp))
-                    .padding(start = 8.dp, end = 8.dp)
-            )
-
-            // Precious Amount Text (₺999,92)
-            Text(
-                text = "₺999,92",
-                fontFamily = FontFamily(Font(R.font.investment_semi_bold)),
-                fontSize = 28.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 4.dp)
-            )
-
-            // Türk Lirası Hesabı
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 16.dp)
-                    .wrapContentHeight()
+                    .fillMaxSize()
+                    .padding(16.dp)
             ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.turkish_flag_16px),
-                    contentDescription = "Türk Lirası Hesabı",
-                    modifier = Modifier.size(16.dp)
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(
-                    text = "Türk Lirası Hesabı",
-                    style = MaterialTheme.typography.bodySmall,
-                    modifier = Modifier.weight(1f)
-                )
-                Text(text = "₺999,92", style = MaterialTheme.typography.bodySmall)
-            }
-
-            // Kıymetli Madenler
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 8.dp)
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_wrap_up_gold_18dp),
-                    contentDescription = "Kıymetli Madenler",
-                    modifier = Modifier.size(18.dp)
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(
-                    text = "Kıymetli Madenler",
-                    style = MaterialTheme.typography.bodySmall,
-                    modifier = Modifier.weight(1f)
-                )
-                Text(text = "__", style = MaterialTheme.typography.bodySmall)
-            }
-
-            // Yatırım Hesabı
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 8.dp)
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_investment_18dp),
-                    contentDescription = "Yatırım Hesabı",
-                    modifier = Modifier.size(18.dp)
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(
-                    text = "Yatırım Hesabı",
-                    style = MaterialTheme.typography.bodySmall,
-                    modifier = Modifier.weight(1f)
-                )
-                Text(text = "__", style = MaterialTheme.typography.bodySmall)
-            }
-
-            // Birikim Hesabı
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 8.dp)
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_no_saving_header_18dp),
-                    contentDescription = "Birikim Hesabı",
-                    modifier = Modifier.size(18.dp)
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(
-                    text = "Birikim Hesabı",
-                    style = MaterialTheme.typography.bodySmall,
-                    modifier = Modifier.weight(1f)
-                )
-                Text(text = "__", style = MaterialTheme.typography.bodySmall)
-            }
-
-            // Info Icon (Bilgi ikonu sağda)
-            Box(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_info_request_money),
-                    contentDescription = "Info Icon",
+                Row(
                     modifier = Modifier
-                        .size(32.dp)
-                        .align(Alignment.TopEnd)
-                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    // Sol taraf (Logo, Tüm Varlıklarım yazısı ve Balance)
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.padding(bottom = 4.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_papara_rounded_gray),
+                                contentDescription = "Logo",
+                                modifier = Modifier.size(16.dp)
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(
+                                text = "Tüm Varlıklarım",
+                                fontFamily = FontFamily(Font(R.font.papara_regular)),
+                                fontSize = 14.sp,
+                                color = Color.Black,
+                                fontWeight = FontWeight.SemiBold
+                            )
+                        }
+                        Text(
+                            text = "₺999,92",
+                            fontFamily = FontFamily(Font(R.font.investment_semi_bold)),
+                            fontSize = 28.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black
+                        )
+                    }
+                    // Sağ taraf (Icon)
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_info_request_money),
+                        contentDescription = "Info Icon",
+                        modifier = Modifier.size(32.dp),
+                    )
+                }
+
+                // Alt kısımdaki hesap bilgileri
+                AccountRow(
+                    iconRes = R.drawable.turkish_flag_16px,
+                    accountName = "Türk Lirası Hesabı",
+                    value = "₺999,92",
+                    accountFontFamily = FontFamily(Font(R.font.investment_medium)),
+                    valueFontFamily = FontFamily(Font(R.font.investment_semi_bold))
+                )
+                AccountRow(
+                    iconRes = R.drawable.ic_wrap_up_gold_18dp,
+                    accountName = "Kıymetli Madenler",
+                    value = "__",
+                    accountFontFamily = FontFamily(Font(R.font.investment_medium)),
+                    valueFontFamily = FontFamily(Font(R.font.investment_semi_bold))
+                )
+                AccountRow(
+                    iconRes = R.drawable.ic_investment_18dp,
+                    accountName = "Yatırım Hesabı",
+                    value = "__",
+                    accountFontFamily = FontFamily(Font(R.font.investment_medium)),
+                    valueFontFamily = FontFamily(Font(R.font.investment_semi_bold))
+                )
+                AccountRow(
+                    iconRes = R.drawable.ic_no_saving_header_18dp,
+                    accountName = "Birikim Hesabı",
+                    value = "__",
+                    accountFontFamily = FontFamily(Font(R.font.investment_medium)),
+                    valueFontFamily = FontFamily(Font(R.font.investment_semi_bold))
                 )
             }
         }
+    }
+}
+
+@Composable
+fun AccountRow(
+    iconRes: Int,
+    accountName: String,
+    value: String,
+    accountFontFamily: FontFamily,
+    valueFontFamily: FontFamily
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 3.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = iconRes),
+            contentDescription = accountName,
+            modifier = Modifier.size(18.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = accountName,
+            fontFamily = accountFontFamily,
+            fontSize = 13.sp,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = value,
+            fontFamily = valueFontFamily,
+            fontSize = 13.sp,
+            color = Color.Black
+        )
     }
 }
